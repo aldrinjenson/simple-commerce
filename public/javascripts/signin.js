024bb0ca47a280c7543324993479f3664e0e695b
Aldrin Jenson <aldrinjenson@gmail.com>
2022-02-20 00:53:10 +0530

    Set up signin

--- public/javascripts/signin.js
+++ public/javascripts/signin.js
@@ -1,13 +1,32 @@
+const email = document.getElementById("email");
+const pass = document.getElementById("pass");
+
 const { createClient } = supabase;
 const _supabase = createClient(
   "https://vfotmyqmvoykyzrkcitv.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTg0ODUwMSwiZXhwIjoxOTU3NDI0NTAxfQ.rG9Wkwqv1BUNWkMuWSOEHPtgaLFRMlno48Nn9iCM8oA"
 );
 
-const handleLogin = async () => {
+const handleSignin = async () => {
+  if (pass.value === "" || email.value === "") {
+    return alert("Please enter all the fields");
+  }
+
   const { user, error } = await _supabase.auth.signIn({
-    email: "example@email.com",
-    password: "example-password",
+    email: email.value,
+    password: pass.value,
   });
-  console.log(user);
+  if (!user || error) {
+    console.log("error in signup: " + error.message);
+    console.log(error);
+
+    return alert(
+      "There is some error in signing in. Please check your details and try again after some time.\n" +
+        error.message
+    );
+  }
+  alert("Successfully signed in");
+  setTimeout(() => {
+    window.location = "products";
+  }, 2000);
 };
