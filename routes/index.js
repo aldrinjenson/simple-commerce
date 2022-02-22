c3dbea198743e7e69c12961ba3ada5df6dc006f9
Aldrin Jenson <aldrinjenson@gmail.com>
2022-02-22 22:04:36 +0530

    Cleanup

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,15 @@ const supabase = createClient(process.env.DB_URL, process.env.PUBLIC_KEY);
 
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
-  // res.render("index", { title: "Express" });
   res.render("signin");
 });
-
-// router.get("/signin", function (req, res, next) {
-//   res.render("signin");
-// });
 
 router.get("/signup", function (req, res, next) {
   res.render("signup");
 });
-router.get("/products", async function (req, res, next) {
+
+router.get("/products", async function (req, res) {
   const { data: products, error } = await supabase.from("products").select();
   if (error) {
     console.log("Error in getting data: " + error);
