6cf040702dab7a3d9fd254e58885f8ff16169ccb
Aldrin Jenson <aldrinjenson@gmail.com>
2022-02-22 21:53:03 +0530

    Add modal for product buy

--- public/javascripts/products.js
+++ public/javascripts/products.js
@@ -1,29 +1,67 @@
-const desc1 = document.querySelector(".description1");
-const readMore1 = document.querySelector(".readMore1");
+const logoutBtn = document.querySelector("#logoutBtn");
+const buyButtons = document.querySelectorAll(".buyBtn");
+const buyFormInputs = document.querySelectorAll(".buyFormInput");
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
 
-readMore1.addEventListener("click", () => {
-  if (readMore1.classList.contains("hide1")) {
-    desc1.style.display = "none";
-    readMore1.innerHTML = "Show More";
-    readMore1.classList.remove("hide1");
-  } else {
-    desc1.style.display = "block";
-    readMore1.innerHTML = "Hide";
-    readMore1.classList.add("hide1");
+const closeModal = () => {
+  modal.style.display = "none";
+  buyFormInputs.forEach((input) => (input.value = ""));
+};
+span.onclick = closeModal;
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal();
   }
+};
+
+const { createClient } = supabase;
+
+const _supabase = createClient(
+  "https://vfotmyqmvoykyzrkcitv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTg0ODUwMSwiZXhwIjoxOTU3NDI0NTAxfQ.rG9Wkwqv1BUNWkMuWSOEHPtgaLFRMlno48Nn9iCM8oA"
+);
+
+const supabaseUser = _supabase.auth.user();
+if (!supabaseUser) {
+  alert("Cannot view products without signing in!");
+  alert("Already logged in!");
+  setTimeout(() => {
+    window.location = "signup";
+  }, 1000);
+}
+
+logoutBtn.addEventListener("click", async () => {
+  const { error } = await _supabase.auth.signOut();
+  if (error) {
+    console.log("Error in signing out: " + error);
+    return alert(
+      "There seems to be some error in signing out. Please try again after a while"
+    );
+  }
+  alert("Successfully signed out.");
+  console.log(_supabase.auth.user());
+  window.location = "signup";
 });
 
-const desc2 = document.querySelector(".description2");
-const readMore2 = document.querySelector(".readMore2");
+let chosenProductId = null;
+for (const btn of buyButtons) {
+  btn.addEventListener("click", () => {
+    chosenProductId = btn.id;
+    modal.style.display = "block";
+  });
+}
 
-readMore2.addEventListener("click", () => {
-  if (readMore2.classList.contains("hide2")) {
-    desc2.style.display = "none";
-    readMore2.innerHTML = "Show More";
-    readMore2.classList.remove("hide2");
-  } else {
-    desc2.style.display = "block";
-    readMore2.innerHTML = "Hide";
-    readMore2.classList.add("hide2");
+const handleBuy = () => {
+  const orderObj = { productId: chosenProductId };
+  for (const buyInput of buyFormInputs) {
+    if (!buyInput.value) {
+      return alert("Please enter a valid value for: " + buyInput.placeholder);
+    }
+    orderObj[buyInput.id] = buyInput.value;
   }
-});
+  console.log(orderObj);
+
+  console.log(chosenProductId);
+};
